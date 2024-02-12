033882becd6c8c2ba471df9ad05227493ec86e10
María <mcuervo@correo.unimet.edu.ve>
2024-02-11 20:17:52 -0400

    Final

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("ipartida").addEventListener("click", function (event) {
       event.preventDefault();
       //Aqui se extrae el valor del input ingresado por el usuario
+      //Validaciones del Nombre
       var jugador1 = document.getElementById("nombre1").value;
       var jugador2 = document.getElementById("nombre2").value;
       var jugador3 = document.getElementById("nombre3").value;
@@ -15,87 +16,68 @@ document.addEventListener("DOMContentLoaded", function () {
         jugador3 == "" ||
         jugador4 == ""
       ) {
-        //Alerta para que los campos no esten vacíos
         alert("Por favor, completa todos los campos requeridos.");
         return;
       }
+      alert("Inicio la partida, buena suerte!");
+  });
 
-      const ganador = verificarGanador();
-      
-      if (ganador) {
-        // Calcular los puntos ganados por el jugador
-        const puntosGanador = calcularPuntos();
-
-        // Llamar a la función agregarUsuarios con el nombre del ganador y los puntos obtenidos
-        agregarUsuarios(ganador.nombre, puntosGanador);
-        alert("El formulario se ha enviado correctamente.");
-
-        // Mostrar puntaje en la tabla
-        mostrarPuntaje(ganador.nombre, puntosGanador);
-
-        // Actualizar la tabla con los usuarios existentes y sus victorias después de agregar un nuevo usuario
-        cargarUsuarios();
-      }
-    });
-
-  function agregarUsuarios(nombre, puntos) {
-    // Obtener el objeto de usuarios del localStorage
+  // Se agrega usuario en localStorage
+  function agregarUsuarios(jugador1, jugador2, jugador3, jugador4) {
     let usuarios = JSON.parse(localStorage.getItem("usuarios")) || {};
 
-    // Incrementar el contador de victorias para el jugador ganador
-    if (usuarios.hasOwnProperty(nombre)) {
-      usuarios[nombre].victorias += 1; 
-      usuarios[nombre].puntos += puntos; 
+    // Verificar si el jugador ya existe en la lista
+    if (usuarios[jugador1]) {
+      usuarios[jugador1]++;
     } else {
-      usuarios[nombre] = {
-        victorias: 1, 
-        puntos: puntos,
-      };
+      usuarios[jugador1] = 1;
     }
 
-    // Guardar el objeto de usuarios actualizado en el localStorage
-    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    if (usuarios[jugador2]) {
+      usuarios[jugador2]++;
+    } else {
+      usuarios[jugador2] = 1;
+    }
 
-    // Llamar a la función cargarUsuariosSelect para actualizar el select
-    cargarUsuariosSelect();
+    if (usuarios[jugador3]) {
+      usuarios[jugador3]++;
+    } else {
+      usuarios[jugador3] = 1;
+    }
+
+    if (usuarios[jugador4]) {
+      usuarios[jugador4]++;
+    } else {
+      usuarios[jugador4] = 1;
+    }
+
+    // Guardar la lista de usuarios actualizada en el localStorage
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    cargarUsuarios();
+    // Mostrar un mensaje indicando que se han agregado los usuarios
+    alert("Usuario agregado correctamente.");
   }
 
-  // Función para cargar usuarios y sus victorias desde localStorage
-  function cargarUsuariosSelect() {
-    // Obtener el objeto de usuarios del localStorage
-    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || {};
-
-    const selectUsuarios = document.getElementById("selectUsuarios");
-    selectUsuarios.innerHTML = ""; // Limpiar contenido previo
+  function cargarUsuarios() {
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || {};
+    const tablaBody = document.querySelector("#tablaVictorias tbody");
+    tablaBody.innerHTML = ""; 
 
     for (const usuario in usuarios) {
-      const opcion = document.createElement("option");
-      opcion.value = usuario;
-      opcion.textContent = `${usuario} - Victorias: ${usuarios[usuario].victorias}, Puntos: ${usuarios[usuario].puntos}`;
-      selectUsuarios.appendChild(opcion);
+      if (usuarios[usuario] > 0) {
+        // Muestra los usuarios a medida que se van obteniendo las victorias
+        const fila = document.createElement("tr");
+        const celdaUsuario = document.createElement("td");
+        celdaUsuario.textContent = usuario;
+        fila.appendChild(celdaUsuario);
+        const celdaVictorias = document.createElement("td");
+        celdaVictorias.textContent = usuarios[usuario];
+        fila.appendChild(celdaVictorias);
+        tablaBody.appendChild(fila); 
+      }
     }
   }
 
-  // Resto del código...
-});
-
-function openTab(evt, tecnologia) {
-  document.getElementById("juga1").style.display = "none";
-  document.getElementById("juga2").style.display = "none";
-  document.getElementById("juga3").style.display = "none";
-  document.getElementById("juga4").style.display = "none";
-
-  document.getElementsByClassName("tablinks")[0].className = "tablinks";
-  document.getElementsByClassName("tablinks")[1].className = "tablinks";
-  document.getElementsByClassName("tablinks")[2].className = "tablinks";
-  document.getElementsByClassName("tablinks")[3].className = "tablinks";
-
-  document.getElementById(tecnologia).style.display = "block";
-  evt.currentTarget.classList.add("active");
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Generación y visualización de cartones de bingo
   function generarNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
@@ -142,10 +124,12 @@ document.addEventListener("DOMContentLoaded", function () {
   function mostrarCarton(carton, numero) {
     const cartonDiv = document.getElementById(`carton${numero}`);
     cartonDiv.innerHTML = "";
+    cartonDiv.style.textAlign = "center";
 
     const tabla = document.createElement("table");
     const titulo = document.createElement("h2");
     titulo.textContent = `Cartón ${numero}`;
+    titulo.style.margin= "auto"
     cartonDiv.appendChild(titulo);
 
     for (let i = 0; i < carton.length; i++) {
@@ -159,7 +143,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     cartonDiv.appendChild(tabla);
 
-    // Ahora, selecciona las celdas después de mostrar el cartón
+    // Se seleccionan las celdas luego de que muestra el cartón
     const cartones = cartonDiv.querySelectorAll("td");
     cartones.forEach(function (celda) {
       if (numerosExtraidos.includes(parseInt(celda.textContent))) {
@@ -169,16 +153,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  document
-    .getElementById("mostrarCarton")
-    .addEventListener("click", function () {
+  document.getElementById("mostrarCarton").addEventListener("click", function () {
       const opcionSeleccionada = this.dataset.opcion;
       const cartonesJugador = generarCarton(opcionSeleccionada);
 
       for (let i = 0; i < cartonesJugador.length; i++) {
         mostrarCarton(cartonesJugador[i], i + 1);
       }
-    });
+  });
 
   document.getElementById("b3").addEventListener("click", function () {
     document.getElementById("mostrarCarton").dataset.opcion = "b3";
@@ -192,48 +174,36 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("mostrarCarton").dataset.opcion = "b5";
   });
 
-  // Sorteo de números de bingo y actualización de cartones
   const numerosExtraidos = [];
+  let salidaNumero = 0;
 
-  // Definimos una variable para contar el número de veces que se ha extraído un número
-  let contadorExtracciones = 0;
-
-  // Definimos la función para extraer un número
-  function extraerNumero() {
-    // Verificamos si ya se han hecho 25 extracciones
-    if (contadorExtracciones >= 25) {
+  // Cantidad de turnos del juego
+  function saleNumero() {
+    if (salidaNumero >= 25) {
       alert("Se acabó la partida!");
-      // Desactivamos el event listener para evitar que se siga ejecutando la función
-      document
-        .getElementById("sacarNumero")
-        .removeEventListener("click", extraerNumero);
+      document.getElementById("sacarNumero").removeEventListener("click", saleNumero);
       return;
-    }
-
-    let numero;
-    do {
-      numero = generarNumeroAleatorio(1, 50);
-    } while (numerosExtraidos.includes(numero));
-
-    // Incrementamos el contador de extracciones
-    contadorExtracciones++;
-
-    // Actualizamos el párrafo que muestra el turno actual
-    document.getElementById(
-      "turno"
-    ).textContent = `Turno: ${contadorExtracciones}`;
-
-    numerosExtraidos.push(numero);
-    document.getElementById(
-      "numeroExtraido"
-    ).textContent = `Número extraído: ${numero}`;
-    statusCartones(numero);
-
-    // Verificar si hay un ganador después de cada extracción
-    verificarGanador();
   }
 
-  // Definimos la función para verificar el estado de los cartones
+  let numero;
+  do {
+      numero = generarNumeroAleatorio(1, 50);
+  } while (numerosExtraidos.includes(numero));
+
+  salidaNumero++;
+
+  //actualiza el turno 
+  document.getElementById("turno").textContent = `Turno: ${salidaNumero}`;
+
+  numerosExtraidos.push(numero);
+  document.getElementById("numeroExtraido").textContent = `Número extraído: ${numero}`;
+  statusCartones(numero);
+
+  //se verifica si hay ganador 
+  winner();
+  }
+
+  //Verifica el estado de los cartones
   function statusCartones(numero) {
     const cartones = document.querySelectorAll(".tabcontent table");
     cartones.forEach(function (carton) {
@@ -247,17 +217,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Agregamos el event listener al botón "Sacar Número de Bingo"
-  document
-    .getElementById("sacarNumero")
-    .addEventListener("click", extraerNumero);
+  // Saca el número de Bingo
+  document.getElementById("sacarNumero").addEventListener("click", saleNumero);
 
-  function verificarGanador() {
-    // Verificar si un jugador ha completado una línea
+  //Determina el ganador al completar la linea
+  function winner() {
     const ganador = comprobarLinea();
     if (ganador) {
       let nombreGanador;
-      let puntosGanador; // Variable para almacenar los puntos del ganador
+      let puntosGanador;
       switch (ganador) {
         case 1:
           nombreGanador = document.getElementById("nombre1").value;
@@ -272,15 +240,14 @@ document.addEventListener("DOMContentLoaded", function () {
           nombreGanador = document.getElementById("nombre4").value;
           break;
         default:
-          // Manejar el caso en el que no se encuentre el nombre del ganador
           alert("No se pudo determinar el nombre del ganador.");
           return null;
       }
+      //muestra el nombre de quien gana
       alert(`¡El jugador ${nombreGanador} ha ganado!`);
-      // Desactivamos el event listener para evitar que se siga ejecutando la función
-      document
-        .getElementById("sacarNumero")
-        .removeEventListener("click", extraerNumero);
+      document.getElementById("sacarNumero").removeEventListener("click", saleNumero);
+      
+      agregarUsuarios(nombreGanador, "", "", "");
       return nombreGanador;
     }
     return null;
@@ -289,11 +256,8 @@ document.addEventListener("DOMContentLoaded", function () {
   function comprobarLinea() {
     const cartones = document.querySelectorAll(".tabcontent table");
     let ganador = null;
-
-    // Declara la variable filas fuera del bucle forEach
     let filas;
 
-    // Verificar líneas horizontales y verticales
     cartones.forEach(function (carton, index) {
       filas = carton.querySelectorAll("tr");
       const columnas = Array.from(
@@ -303,7 +267,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
       filas.forEach(function (fila) {
         let countHorizontal = 0;
-        let countVertical = 0;
 
         fila.querySelectorAll("td").forEach(function (celda) {
           if (celda.innerHTML.includes("s")) {
@@ -330,7 +293,7 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
 
-    // Verificar diagonales
+    // Verifica diagonales
     if (!ganador) {
       const primeraDiagonal = [];
       const segundaDiagonal = [];
@@ -359,35 +322,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Función de reinicio del juego
   function reiniciarJuego() {
-    // Reiniciar variables y elementos del juego
     numerosExtraidos.length = 0;
-    contadorExtracciones = 0;
+    salidaNumero = 0;
     document.getElementById("numeroExtraido").textContent = "Número extraído:";
     document.getElementById("turno").textContent = "Turno: 0";
 
-    // Restaurar celdas de cartones
     const celdasCartones = document.querySelectorAll(".tabcontent table td");
     celdasCartones.forEach(function (celda) {
       celda.innerHTML = "";
     });
 
-    // Limpiar nombres de los inputs
     document.getElementById("nombre1").value = "";
     document.getElementById("nombre2").value = "";
     document.getElementById("nombre3").value = "";
     document.getElementById("nombre4").value = "";
 
-    // Reactivar el botón de "Sacar Número de Bingo"
-    document
-      .getElementById("sacarNumero")
-      .addEventListener("click", extraerNumero);
+    document.getElementById("sacarNumero").addEventListener("click", saleNumero);
 
-    // Actualizar la tabla de usuarios y sus victorias después de reiniciar el juego
+    const ganador = winner();
+    if (ganador) {
+      agregarUsuarios(ganador, "", "", "");
+    }
+
     cargarUsuarios();
   }
 
-  // Agregar evento al botón de reinicio
-  document
-    .getElementById("reiniciarJuego")
-    .addEventListener("click", reiniciarJuego);
+  document.getElementById("reiniciarJuego").addEventListener("click", reiniciarJuego);
 });
+
+function openTab(evt, tecnologia) {
+  document.getElementById("juga1").style.display = "none";
+  document.getElementById("juga2").style.display = "none";
+  document.getElementById("juga3").style.display = "none";
+  document.getElementById("juga4").style.display = "none";
+
+  document.getElementsByClassName("tablinks")[0].className = "tablinks";
+  document.getElementsByClassName("tablinks")[1].className = "tablinks";
+  document.getElementsByClassName("tablinks")[2].className = "tablinks";
+  document.getElementsByClassName("tablinks")[3].className = "tablinks";
+
+  document.getElementById(tecnologia).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
